83a59b5b1c7812868ab176989087178fe3a31ef9
utkarsh-1997 <ag.utkarsh1997@gmail.com>
2022-03-07 22:57:44 +0530

    Added Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Rating from "./Rating";
 
 function Product(props) {
   const { product } = props;
@@ -13,6 +14,7 @@ function Product(props) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>₹{product.price}</Card.Text>
         <Button>Add to cart</Button>
       </Card.Body>

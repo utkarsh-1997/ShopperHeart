122b1579ab5870b80c43ae1c4e27766c5d92ee96
utkarsh-1997 <ag.utkarsh1997@gmail.com>
2022-03-08 22:00:26 +0530

    Created Loading and Message Component

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 # Steps used by me:
 
-1. Introduction
-2. Install Tools
+1. Checked and Researched the requirements.
+2. Installed Tools
 3. Create React App
 4. Create Git Repository
 5. List Products
@@ -48,3 +48,8 @@
 12. Create Product Details Screen
     1. fetch product from backend
     2. create 3 columns for image, info and action
+13. Create Loading and Message Component
+    1. create loading component
+    2. use spinner component
+    3. craete message component
+    4. create utils.js to define getError function
---
+++ frontend/src/components/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: "Blackberry Slim Shirt",
+      slug: "blackberry-slim-shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg", // Took sample images from Snapdeal 850x995px
+      price: 120,
+      countInStock: 10,
+      brand: "Blackberry",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Levis Fit Shirt",
+      slug: "levis-fit-shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 250,
+      countInStock: 20,
+      brand: "Levis",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Blackberry Slim Pant",
+      slug: "blackberry-slim-pant",
+      category: "Pants",
+      image: "/images/p3.jpg",
+      price: 25,
+      countInStock: 15,
+      brand: "Blackberry",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality pant",
+    },
+    {
+      name: "Levis Fit Pant",
+      slug: "levis-fit-pant",
+      category: "Pants",
+      image: "/images/p4.jpg",
+      price: 65,
+      countInStock: 5,
+      brand: "Levis",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality pant",
+    },
+  ],
+};
+
+export default data;
